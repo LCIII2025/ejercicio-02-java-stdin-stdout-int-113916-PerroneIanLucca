153c2d00d38a7777d21c2165755430855c8981fe
113916_PerroneIanLucca <113916@tecnicatura.frc.utn.edu.ar>
2025-04-17 20:51:44 -0300

    Update App.java

--- src/main/java/ar/edu/utn/frc/tup/lciii/App.java
+++ src/main/java/ar/edu/utn/frc/tup/lciii/App.java
@@ -12,10 +12,12 @@ public class App
     {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
-        // Complete this line
-        // Complete this line
+        int b = scan.nextInt();
+        int c = scan.nextInt();
 
         System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
         // Complete this line
         // Complete this line
 
